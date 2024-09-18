a1f8b0e21346476351cea328536de23bb65372d9
rafaelakrone <161483510+rafaelakrone@users.noreply.github.com>
2024-09-18 14:48:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,56 @@
+criaCartao(
+    'historia',
+    'quem foi o primeiro presidente dos EUA ?',
+    'George Washington'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'Geografia ?',
+    'O planeta mais proximo so Sol e Mercurio'
+)
+
+criaCartao(
+    'Biologia',
+    'O que e fotossintese ?',
+    'Fotossintese e o processo pelo qual as plantas produzem energia a partir da luz solar'
+) 
+criaCartao(
+    'historia',
+    'Em que ano ocorreu a Revolucao Francesa ?',
+    'Ocorreu em 1789'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o rio mais longo do mundo?',
+    'Rio Nilo'
+
+)
+criaCartao(
+    'Curiosidade',
+    'Qual o alimento mais consumido no mundo ?',
+    'Arroz'
+)
+criaCartao(
+    'Ciencias',
+    'O que e a camada de ozonio?',
+    'uma regiao da estratosfera que protege a Terra dos raios ultravioleta do Sol'
+
+)
+criaCartao(
+    'Curiosidade',
+    'Qual o unico mamifero capaz de voar ?',
+    'Morcego'
+)
+criaCartao(
+    'Ciencias',
+    'O que sao as celulas tronco?',
+    'sao celulas que tem a capacidade de se transformar em varioa tipos de celulas do corpo '
+)
